6bc1c0804e430cf30fb06e632c62e7d42e0a7b3c
Hjalte <h.jaltehb123@gmail.com>
2024-07-24 19:22:50 +0200

    Added Cors functionality for local testing and updated Batch model

--- Models/Batch.cs
+++ Models/Batch.cs
@@ -3,7 +3,10 @@ namespace Kombucha.Models;
 public class Batch {
     public long Id { get; set; }
     public DateOnly StartTime { get; set; }
+    public string SugarKind { get; set; }
     public int Sugar { get; set; }
     public string Tea { get; set; }
+    public int GramsOfTea { get; set; }
+    public int SteepMinutes { get; set; }
     public string Description { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -7,6 +7,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<KombuchaContext>(opt => opt.UseInMemoryDatabase("KombuchaDB"));
+
+builder.Services.AddCors(options => {
+    options.AddPolicy("AllowReactApp", builder => {
+        builder.WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -18,9 +27,14 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseCors("AllowReactApp");
 
 app.UseAuthorization();
 
